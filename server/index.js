94c336f536810f2fb2b12f884a9d150248416001
Billie Tran <billietran.developer@gmail.com>
2022-05-05 00:30:12 -0700

    Added gitignore folder

--- server/index.js
+++ server/index.js
@@ -10,8 +10,8 @@ app.use(express.json())
 const db = mysql.createConnection({
     user: 'root',
     host: 'localhost',
-    // password: ,
-    // database: ''
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.DATABASE_NAME,
 })
 
 
