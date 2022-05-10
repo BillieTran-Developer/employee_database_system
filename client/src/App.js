5a679879a2024cc6d791fe247cabde0e9b7dd851
Billie Tran <billietran.developer@gmail.com>
2022-05-10 00:05:56 -0700

    Connected client to server with add function.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,53 @@
-
+import {useState} from 'react';
+import Axios from 'axios'
 import './App.css';
 
 function App() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [position, setPosition] = useState('');
+  const [salary, setSalary] = useState(0);
+
+  const [employeeList, setEmployeeList] = useState([])
+
+  const addEmployee = () => {
+    const employee = {
+      firstName,
+      lastName,
+      position,
+      salary
+    }
+    Axios.post(`http://localhost:3001/add`, employee)
+      .then(() => {
+      setEmployeeList([...employeeList, employee])
+    }).catch(e => {
+      console.log(e)
+    })
+  }
+
   return (
     <div className="App">
-    
+        <div className='information'>
+          <label>
+            <span>First Name:</span>
+            <input type="text" onChange={(e) => setFirstName(e.target.value)}/> 
+          </label>
+          <label>
+            <span>Last Name:</span>
+            <input type="text" onChange={(e) => setLastName(e.target.value)}/>
+          </label>
+          <label>
+            <span>Position:</span>
+            <input type="text" onChange={(e) => setPosition(e.target.value)}/>
+          </label>
+          <label>
+            <span>Salary:</span>
+            <input type="numer" onChange={(e) => setSalary(e.target.value)}/>
+          </label>
+          <button onClick={addEmployee}>
+            Add Employee
+          </button>
+      </div>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const mysql = require('mysql');
@@ -10,10 +11,26 @@ app.use(express.json())
 const db = mysql.createConnection({
     user: 'root',
     host: 'localhost',
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.DATABASE_NAME,
+    password: `${process.env.MYSQL_PASSWORD}`,
+    database: `${process.env.DATABASE_NAME}`,
 })
 
+app.post('/add', (req, res) => {
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const position = req.body.position;
+    const salary = req.body.salary;
+    
+    db.query(`INSERT INTO employees (first_name, last_Name, position, salary)
+        VALUES(?, ?, ?, ?)`, 
+        [firstName, lastName, position, salary], (err, result) => {
+            if(err) {
+                console.log(err)
+            } else {
+                res.send(result)
+            }
+        })
+})
 
 app.listen(PORT, () => {
     console.log(`CONNECTED TO ${PORT}`)
